e17052e07757c179d9c9de67dc359f619da11db5
Maximilian Torggler <inquiries@maximilian-torggler.dev>
2021-10-27 16:39:19 +0200

    feat: cardano hipsters og image

--- api/_lib/types.ts
+++ api/_lib/types.ts
@@ -1,13 +1,6 @@
-export type FileType = 'png' | 'jpeg';
-export type Theme = 'light' | 'dark';
+export type FileType = "png" | "jpeg";
+export type Theme = "light" | "dark";
 
 export interface ParsedRequest {
-    fileType: FileType;
-    text: string;
-    theme: Theme;
-    md: boolean;
-    fontSize: string;
-    images: string[];
-    widths: string[];
-    heights: string[];
+  imageUrl: string;
 }
